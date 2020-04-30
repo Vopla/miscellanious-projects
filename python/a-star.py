c3fe852cfef06e847dcb51a405995b48d3d52902
Aleksis <allu.96.aleksis@gmail.com>
2020-04-30 23:31:17 +0300

    add grid coordinates to a-star.py

--- python/a-star.py
+++ python/a-star.py
@@ -11,9 +11,6 @@ class Grid:
         self.nodeAmount = 0
         self.nodeList = []
 
-    def getListItem(self, x, y):
-        return self.nodeList[x][y]
-
 class Node:
 
     def __init__(self, x, y, weight):
@@ -25,7 +22,7 @@ class Node:
         self.parent = None
     
     def checkNeighbors(self, x, y):
-        
+        return
 
     def setLoc(self, x, y):
         self.x = x
@@ -42,9 +39,10 @@ pygame.init()
 Loop = True
 
 WHITE = (255,255,255)
-BLACK = (0,0,0)
+BLACK = (70,70,70)
+GREEN = (0,180,0)
 
-screenSize = width, height = 640, 480
+screenSize = width, height = 640, 640
 
 sqHeight = int(height/10)
 sqWidth = int(width/10)
@@ -52,45 +50,60 @@ sqWidth = int(width/10)
 screen = pygame.display.set_mode(screenSize)
 pygame.display.set_caption("A* Pathfinding")
 
-background = pygame.Surface(screen.get_size())
+font = pygame.font.SysFont(None, 32)
 
 grid = Grid(int(width), int(height), sqWidth, sqHeight)
 
+fps = pygame.time.Clock()
+
 while Loop:
 
-    background.fill(BLACK)
+    screen.fill(GREEN)
 
     for y in range(grid.height // sqHeight):
         for x in range(grid.width // sqWidth):
             node = Node(x, y, rand.randint(1,20))
             grid.nodeList.append(node)
-            nodeFill = pygame.Rect(node.x * sqWidth, node.y * sqHeight, sqWidth, sqHeight)
-            pygame.draw.rect(screen, WHITE, nodeFill, 2)
             grid.nodeAmount += 1
 
+    for node in grid.nodeList:
+        nodeFill = pygame.Rect(node.x * sqWidth, node.y * sqHeight, sqWidth, sqHeight)
+
+        text = font.render(f"{node.x}, {node.y}", 1, WHITE)
+        textRect = text.get_rect()
+        textRect.centerx = nodeFill.centerx
+        textRect.centery = nodeFill.centery
+        screen.blit(text, textRect)
+        
+        if node.nodeVisited:            
+            pygame.draw.rect(screen, WHITE, nodeFill, 2)       
+        else:
+            pygame.draw.rect(screen, BLACK, nodeFill, 2)
+
     for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
+
         if event.type == pygame.QUIT: 
             Loop = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
 
-            column = pos[0] // sqHeight
-            row = pos[1] // sqWidth
+            column = (pos[0] // sqHeight)
+            row = (pos[1] // sqWidth)
 
-            #currentSquare = grid.getListItem(column, row)
+            for node in grid.nodeList:
+                if node.x == row and node.y == column:
+                    if node.nodeVisited == False:
+                        node.nodeVisited = True
+                        print(f"square at location {row}, {column} has been visited.")
+                        break
+                    
+                    elif node.nodeVisited:
+                        node.nodeVisited = False
+                        print(f"square at location {row}, {column} has been not visited.")
+                        break
 
-            print(column, row, node.weight)
-
-            #outline = pygame.Rect(grid.getList((node.x *sqWidth), (node.y * sqHeight), sqWidth+2, sqHeight+2))
-
-            #if grid.nodeList[column][row].node.nodeObstacle is True:
-            #    node.nodeObstacle == False
-
-            #else:
-            #    node.nodeObstacle == True
-
-
+    fps.tick(60)
     pygame.display.flip()
 
 pygame.quit()
